e319d36a0088d07f5dad32d6d15dbb6861ceffde
Romain Dreuilhet <romain.dreuilhet@ratp.fr>
2023-03-24 12:06:53 +0100

    Add a fully commented script that clone, install and run the project. It has to be uncommented to be run

---
+++ all_cmd.sh
@@ -0,0 +1,13 @@
+##!/bin/bash
+#git clone https://github.com/romain420/camera_recorder.git
+#cd cam_recorder
+#
+## Create a new virtual environment with Python 3.10
+#python3.10 -m venv venv
+#source venv/bin/activate
+#pip install --upgrade pip
+## Install all requirements
+#pip install -r requirements.txt
+#
+## To run recording scrip
+#python main.py -d 0 -p './outputs' -f mp4
